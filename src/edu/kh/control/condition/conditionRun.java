0ffd50ae4471ececc4d35acfdd3e94d674102516
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-07-17 17:56:56 +0900

    17

---
+++ src/edu/kh/control/condition/conditionRun.java
@@ -0,0 +1,18 @@
+package edu.kh.control.condition;
+
+public class conditionRun {
+	public static void main(String[] args) {
+		
+		conditionExample condition = new conditionExample();
+		
+		//condition.ex1();
+		//condition.ex2();
+		//condition.ex3();
+		//condition.ex4();
+		condition.ex5();
+		
+		
+		
+	}
+
+}
