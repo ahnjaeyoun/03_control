dbd23333efb6be371ebce0db8a701b2116ecbec9
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-07-19 00:07:21 +0900

    home t

--- src/edu/kh/control/condition/ConditionExample.java
+++ src/edu/kh/control/condition/ConditionExample.java
@@ -2,7 +2,9 @@ package edu.kh.control.condition;
 
 import java.util.Scanner;
 
-public class conditionExample {
+public class ConditionExample {
+	
+	Scanner sc = new Scanner(System.in);
 	
 	public void ex1() {
 		
@@ -12,28 +14,29 @@ public class conditionExample {
 		/*
 		 * [작성법]
 		 * if(조건식) {
-		 *        조건식이 true 일 때 수행할 코드
-		 * }
+		 * 		조건식이 true 일 때 수행할 코드
+		 * } 
 		 * 
 		 * 
-		 */
+		 * */
 		
-		Scanner sc = new Scanner(System.in);
+
 		
 		System.out.print("정수 입력 : ");
 		int input = sc.nextInt();
 		
 		// 입력된 정수가 양수인지 검사
 		if(input > 0) {
-			System.out.print("양수입니다");
+			System.out.println("양수입니다");
 		}
 		
 		if(input <= 0) {
 			System.out.println("양수가 아니다.");
 		}
 		
+	
 	}
-
+	
 	public void ex2() {
 		
 		// if - else 문
@@ -41,37 +44,37 @@ public class conditionExample {
 		// false면 else 구문이 수행됨.
 		
 		/*
-		 * if(조건식){
-		 *       조건식이 true일 때 수행될 코드 
+		 * if(조건식) {
+		 * 		조건식이 true일 때 수행될 코드
 		 * } else {
-		 *       조건식이 false일 때 수행될 코드
+		 *      조건식이 false일 떄 수행될 코드
 		 * }
 		 * 
 		 * 
-		 */
-		
-		Scanner sc = new Scanner(System.in);
+		 * 
+		 * */
 		
 		// 홀짝 검사
 		System.out.print("정수 입력 : ");
 		int input = sc.nextInt();
 		
-		if( input % 2 != 0) {
-			System.out.println("홀수입니다. ");
-			
+		if( input % 2 != 0 ) {
+			System.out.println("홀수 입니다.");
+		
 		} else { // 짝수 또는 0 입력 시 수행
 			
-			// 중첨 if문
+			// 중첩 if문
 			if(input == 0) {
-				System.out.println("0입니다.");
+				System.out.println("0 입니다.");
 			} else {
-				System.out.println("짝수입니다.");
+				System.out.println("짝수 입니다.");
 			}
+			
+			
 		}
 		
 		
 	}
-	
 	
 	public void ex3() {
 		
@@ -79,9 +82,8 @@ public class conditionExample {
 		
 		// if - else if - else
 		
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("정수 입력 : ");
+		System.out.print("정수 입력 : ");
 		int input = sc.nextInt();
 		
 		if(input > 0) { // input이 양수일 경우
@@ -93,56 +95,53 @@ public class conditionExample {
 			
 		} else {
 			System.out.println("0 입니다.");
+			
 		}
 		
 		
 		
-		
-		
-		
-		
 	}
-	
-	
+		
 	public void ex4() {
 		
 		// 달(month)을 입력받아 해당 달에 맞는 계절 출력
-		// 단, 겨울일때 온도가 -15도 이하 "한파경보", -12도 이해 "한파 주의보"
-		// 여름일 때 온도가 35도 이상 "폭염 경보", 33도 이상 " 폭염 주의보"
+		// 단, 겨울일때 온도가 -15도 이하 "한파 경보", -12이하 "한파 주의보"
+		// 여름일때 온도가 35도 이상 "폭염 경보", 33도 이상 "폭염 주의보"
 		// 1~12 사이가 아닐땐 "해당하는 계절이 없습니다" 출력
 		
-		Scanner sc = new Scanner(System.in);
+	
 		
 		System.out.print("달 입력 : ");
 		int month = sc.nextInt();
 		
-	    int temperature = 37;
-	    
-	    String season; // 아래 조건문 수행 결과를 저장할 변수 선언
+		int temperature = 34;
+		
+		String season; // 아래 조건문 수행 결과를 저장할 변수 선언
 		
 		if(month == 1 || month == 2 || month == 12) {
 			season = "겨울";
 			
 			if(temperature <= -15) {
-				// season = "겨울 한파 경보";
+				//season = "겨울 한파 경보";
 				season += " 한파 경보";
-				// season = season + "한파 경보"
+				// season = season + " 한파 경보" 
 			} else if( temperature <= -12 ) {
 				season += " 한파 주의보";
-				
-				
 			}
 			
-		}else if( month >= 3 && month <= 5) {
+			
+			
+		} else if( month >= 3 && month <= 5) {
 			season = "봄";
 		} else if( month >= 6 && month <= 8) {
 			season = "여름";
 			
 			if(temperature >= 35) {
 				season += " 폭염 경보";
-			} else if(temperature >= 33) {
+			} else if(temperature >= 33 ) {
 				season += " 폭염 주의보";
 			}
+			
 			
 			
 			
@@ -155,38 +154,35 @@ public class conditionExample {
 		System.out.println(season);
 		
 		
-		
-		}
+	}
 	
 	public void ex5() {
-		
 		// 나이를 입력 받아
 		// 13세 이하면 "어린이 입니다."
 		// 13세 초과 19세 이하면 : "청소년 입니다."
 		// 19세 초과 시 : "성인 입니다". 출력
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("나이 : ");
+		System.out.print("나이 입력 : ");
 		int age = sc.nextInt();
 		
 		if(age <= 13) {
 			System.out.println("어린이 입니다.");
-		
-		} else if(13 < age && age <= 19) {
-			System.out.println("청소년 입니다.");
-		
+			
 		} else if(age > 19) {
-			System.out.println("성인 입니다.");
+			System.out.println("성인입니다.");
+			
+		} else {
+			System.out.println("청소년입니다.");
+			
 		}
 		
-	
+		
+		
 		
 		
 	}
 	
 	public void ex6() {
-		
 		// 점수(100점 만점)를 입력 받아
 		// 90점 이상 : A
 		// 80점 이상 90점 미만 : B
@@ -194,76 +190,75 @@ public class conditionExample {
 		// 60점 이상 70점 미만 : D
 		// 60점 미만 : F
 		// 0점 미만, 100 초과 : "잘못 입력하셨습니다"
+
+		System.out.print("점수 입력(0~100) : ");
+		int score = sc.nextInt();
 		
-		Scanner sc = new Scanner(System.in);
+		String result;
 		
-		System.out.print("점수(100점 만점) : ");
-		int sco = sc.nextInt();
-		
-	    if(sco >= 90 && sco <= 100) {
-			System.out.println("A");
-		
-	    } else if(sco >= 80 && sco < 90) {
-			System.out.println("B");
-		
-	    } else if(sco >= 70 && sco < 80) {
-			System.out.println("C");
-		
-	    } else if(sco >= 60 && sco < 70) {
-			System.out.println("D");
-		
-	    } else if(sco >= 0 && sco < 60) {
-			System.out.println("F");
-		
-	    } else {
-			System.out.println("잘못 입력하셨습니다.");
-		
-	    }
-		
+		if(score < 0 || score > 100) {
+			result = "잘못 입력하셨습니다!";
 			
+		} else if( score >= 90 ) {
+			result = "A";
+			
+		} else if( score >= 80 ) {
+			result = "B";
+			
+		} else if( score >= 70 ) {
+			result = "C";
+			
+		} else if( score >= 60 ) {
+			result = "D";
+			
+		} else {
+			result = "F";
+		}
+		
+		
+		System.out.println(result);
 		
 		
 	}
 	
 	public void ex7() {
-		
 		// 놀이기구 탑승 제한 검사
 		// 나이가 12세 이상, 키 140.0cm 이상 일 경우에만 "탑승 가능"
 		// 나이가 12미만인 경우 : "적정 연령이 아닙니다."
 		// 키가 140.0cm 미만 : "적정 키가 아닙니다."
 		// 나이를 0세 미만, 100세 초과 시 : "잘못 입력 하셨습니다."
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("나이 : ");
+		System.out.print("나이 입력 : ");
 		int age = sc.nextInt();
 		
-		System.out.print("키 : ");
-		double hei = sc.nextDouble();
+		String result;
 		
-		
-		if(age >= 12 && hei >= 140.0) {
-			System.out.println("탑승 가능");
+		if(age < 0 || age > 100) {
+			result = "잘못 입력하셨습니다!";
 			
-		} else if(age < 12) {
-			System.out.println("적정 연령이 아닙니다.");
-		
-		} else if(hei < 140.0) {
-			System.out.println("적정 키가 아닙니다.");
+		} else {
+			System.out.print("키 입력 : ");
+			double height = sc.nextDouble();
 			
-		} else if (age < 0 || age > 100) {
-			System.out.println("잘못 입력 하셨습니다");
-			
+			if (age < 12) {
+				result = "적정 연령이 아닙니다";
+				
+			} else if( height < 140.0) {
+				result = "적정 키가 아닙니다";
+				
+			} else {
+				result = "탑승 가능";
+				
+			}
 		}
 		
-		
-		
+		System.out.println(result);
 		
 		
 	}
 	
+	
 	public void ex8() {
-		
 		// 놀이기구 탑승 제한 검사 프로그램
 		// 조건 - 나이 : 12세 이상
 		// - 키 : 140.0cm 이상
@@ -274,38 +269,42 @@ public class conditionExample {
 		// 나이 X , 키 O : "키는 적절하나, 나이는 적절치 않음";
 		// 나이 X , 키 X : "나이와 키 모두 적절치 않음";
 		// 나이 O , 키 O : "탑승 가능"
-	
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("나이 : ");
+		
+		System.out.print("나이 입력 : ");
 		int age = sc.nextInt();
 		
-		System.out.print("키 : ");
-		double hei = sc.nextDouble();
+		String result;
 		
-		if(age < 0 || age > 100) {
-			System.out.println("나이를 잘못 입력 하셨습니다.");
+		if( age < 0 || age > 100 ) {
+			result = "나이를 잘못 입력하셨습니다.";
 			
-		} else if(hei < 0 || hei > 250.0) {
-			System.out.println("키를 잘못 입력 하셨습니다.");
+		} else {
 			
-		} else if(age >= 12 && hei < 140.0) {
-			System.out.println("나이는 적절하나, 키가 적절치 않음");
+			System.out.print("키 입력 : ");
+			double height = sc.nextDouble();
 			
-		} else if(age < 12 && hei >= 140.0) {
-			System.out.println("키는 적절하나, 나이는 적절치 않음");
-			
-		} else if(age < 12 && hei < 140.0) {
-			System.out.println("나이와 키 모두 적절치 않음");
-			
-		} else if(age >= 12 && hei >= 140.0) {
-			System.out.println("탑승 가능");
-			
+			if(height < 0 || height > 250) {
+				result = "키를 잘못 입력하셨습니다.";
+				
+			} else {
+				
+				if(age < 12 && height >= 140.0) { // 나이 X, 키 O
+					result = "키는 적절하나, 나이가 적절치 않음"; 
+							
+				} else if( age >= 12 && height < 140.0 ) { // 나이 O, 키 X
+					result = "나이는 적절하나, 키가 적절치 않음";
+					
+				} else if( age < 12 && height < 140.0 ) {
+					result = "나이와 키 모두 적절치 않음";
+				
+				} else {
+					result = "탑승 가능";
+				}
+				
+			}
 		}
-		
-		
-		
-		
+		System.out.println(result);
 		
 		
 		
@@ -313,5 +312,10 @@ public class conditionExample {
 		
 		
 	}
+	
+	
+	
+	
+	
 	
 }
---
+++ src/edu/kh/control/loop/LoopRun.java
@@ -0,0 +1,17 @@
+package edu.kh.control.loop;
+
+import java.util.Iterator;
+
+public class LoopRun {
+	public static void main(String[] args) {
+		
+		ForExample forEx = new ForExample();
+		//forEx.ex1();
+		//forEx.ex3();
+		//forEx.ex6();
+		//forEx.ex7();
+		//forEx.ex8();
+		//forEx.ex13();
+		forEx.ex14();
+	}
+}
