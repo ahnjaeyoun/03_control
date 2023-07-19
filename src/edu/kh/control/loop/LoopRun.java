f3d007004c7e659c21af7905e0949ab32839d8be
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-07-19 17:07:29 +0900

    academy0719

--- src/edu/kh/control/loop/LoopRun.java
+++ src/edu/kh/control/loop/LoopRun.java
@@ -12,6 +12,29 @@ public class LoopRun {
 		//forEx.ex7();
 		//forEx.ex8();
 		//forEx.ex13();
-		forEx.ex14();
+		//forEx.ex14();
+		//forEx.ex16();
+		//forEx.ex17();
+		//forEx.ex18();
+		//forEx.ex19();
+		//forEx.ex20();
+		//forEx.ex21();
+		//forEx.ex22();
+		//forEx.ex23();
+		//forEx.ex24();
+		//forEx.ex25();
+		//forEx.ex26();
+		//forEx.ex27();
+		//forEx.ex28();
+		//forEx.ex29();
+		
+		WhileExample whileEx = new WhileExample();{
+		//whileEx.ex1();	
+		//whileEx.ex2();	
+		//whileEx.ex3();	
+			
+			
+			
+		}
 	}
 }
